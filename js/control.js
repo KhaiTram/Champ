d16a41e26b1611784003fc0dc5cfe51f0c3edb31
bruce <k-h.t@hotmail.com>
2020-06-19 11:01:13 +0200

    updated start page

--- js/control.js
+++ js/control.js
@@ -1,4 +1,19 @@
-$(function(){
-    $("#khai").load("khai.html"); 
-  });
+var onScrollHandler = function () {
+  stopParallax("beine");
+  stopParallax("brust");
+  stopParallax("bauch");
+  stopParallax("arme");
 
+};
+window.addEventListener("scroll", onScrollHandler);
+
+function stopParallax(id) {
+  var beine = document.getElementById(id);
+  var beinePosition = beine.getBoundingClientRect()
+  if (beinePosition.y + 150 > window.innerHeight / 2) {
+    beine.style.backgroundAttachment = "fixed";
+  }
+  else {
+    beine.style.backgroundAttachment = "scroll";
+  }
+}
